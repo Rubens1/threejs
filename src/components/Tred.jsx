98dd530c25f5b41ab8db4471bb8e57b6e84bc05a
Rubens Nogueira <rubens.jesus1997@gmail.com>
2024-05-09 16:55:33 -0300

    03E8873FE62F2ADFD6B3BB16A2EB587C9526F74C32BA0ACD963AAA928022924A7A0269A3A45FA0AD2B34F0DF5AB229CEF314E1FCEE0B7D4A722B7E66B5D7717B
    03E8873FE62F2ADFD6B3BB16A2EB587C9526F74C32BA0ACD963AAA928022924A7A0269A3A45FA0AD2B34F0DF5AB229CEF314E1FCEE0B7D4A722B7E66B5D7717B

--- src/components/Tred.jsx
+++ src/components/Tred.jsx
@@ -6,86 +6,96 @@ import { TextureLoader } from 'three/src/loaders/TextureLoader.js';
 
 export default function Tred() {
     const canvasRef = useRef(null);
-    const [texture, setTexture] = useState(null); // Estado para armazenar a textura carregada
+    const [imagem, setImagem] = useState(null); // Estado para armazenar a textura carregada
 
     useEffect(() => {
-        const ambientLight = new THREE.AmbientLight(0xFFFFFF, 1); // Luz ambiente com intensidade máxima
-        const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1); // Luz direcional com intensidade máxima
+    const ambientLight = new THREE.AmbientLight(0xFFFFFF, 1); // Luz ambiente com intensidade máxima
+    const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1); // Luz direcional com intensidade máxima
 
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(5, window.innerWidth / window.innerHeight, 0.1, 1000);
-        const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current, alpha: true }); // Set alpha to true for transparent background
-        renderer.setSize(window.innerWidth, window.innerHeight);
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(5, window.innerWidth / window.innerHeight, 0.1, 1000);
+    
+    // Aqui você pode definir as dimensões do canvas
+    const canvas = canvasRef.current;
+    const renderer = new THREE.WebGLRenderer({ canvas, alpha: true }); // Set alpha to true for transparent background
+    renderer.setSize(800, 500); // Define o tamanho do canvas
+    //renderer.setSize(window.innerWidth, window.innerHeight);
+    
+    const textureLoader = new TextureLoader();
+    const texture = textureLoader.load('mesa.png'); // Carrega a textura da imagem
+  
+   // Criar um plano para o cenário
+    const geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide, wireframeLinewidth: 2 });
+    const cenario = new THREE.Mesh(geometry, material);
 
-        const textureLoader = new TextureLoader();
-        const texture = textureLoader.load('mesa.png'); // Carrega a textura da imagem
-      
-       // Criar um plano para o cenário
-        const geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide, wireframeLinewidth: 2 });
-        const cenario = new THREE.Mesh(geometry, material);
+    // Posicionar e rotacionar o cenário
+    cenario.rotation.x = -Math.PI / 2; // Rotaciona o plano para que fique no plano horizontal
+    cenario.position.y = -0.032; // Ajusta a posição vertical do cenário
 
-        // Posicionar e rotacionar o cenário
-        cenario.rotation.x = -Math.PI / 2; // Rotaciona o plano para que fique no plano horizontal
-        cenario.position.y = -0.032; // Ajusta a posição vertical do cenário
+    // Adicionar o cenário à cena
+    scene.add(cenario);
 
-        // Adicionar o cenário à cena
-        scene.add(cenario);
+    const loader = new GLTFLoader();
 
-        const loader = new GLTFLoader();
+    scene.add(ambientLight);
+    scene.add(directionalLight);
 
-        scene.add(ambientLight);
-        scene.add(directionalLight);
+    loader.load(
+        'cup/scene.gltf',
+        (gltf) => {
+            gltf.scene.traverse((child) => {
+                if (child.isMesh) {
+                    // Altere as propriedades do material aqui
+                    console.log(child.material.map);
+                    child.material.map = imagem;
+                    child.material.needsUpdate = true;
+                }
+            });
+            scene.add(gltf.scene);
+        },
+        (xhr) => {
+            //console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        },
+        (error) => {
+            //console.error('Error loading GLTF:', error);
+        }
+    );
 
-        loader.load(
-            'cup/scene.gltf',
-            (gltf) => {
-                gltf.scene.traverse((child) => {
-                    if (child.isMesh) {
-                        if (child.material.map) {
-                            child.material.needsUpdate = true;
-                        }
-                    }
-                });
-                scene.add(gltf.scene);
-            },
-            (xhr) => {
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-            },
-            (error) => {
-                console.error('Error loading GLTF:', error);
-            }
-        );
+    camera.position.z = 5;
 
-        camera.position.z = 5;
+    const controls = new OrbitControls(camera, renderer.domElement);
 
-        const controls = new OrbitControls(camera, renderer.domElement);
+    const animate = () => {
+        requestAnimationFrame(animate);
+        controls.update(); // Update controls in the animation loop
+        renderer.render(scene, camera);
+    };
 
-        const animate = () => {
-            requestAnimationFrame(animate);
-            controls.update(); // Update controls in the animation loop
-            renderer.render(scene, camera);
-        };
+    animate();
 
-        animate();
+}, [imagem]);
 
-    }, []);
-
-    /* useEffect(() => {
+    useEffect(() => {
         // Função para carregar a textura quando o componente for montado
         const textureLoader = new TextureLoader();
         textureLoader.load(
             'urso.png',
             (texture) => {
                 
-                console.log('Texture loaded:', texture);
-                setTexture(texture); // Define a textura carregada no estado
+                //console.log('Texture loaded:', texture);
+                setImagem(texture); // Define a textura carregada no estado
             },
+            undefined,
             (error) => {
-                console.error('Error loading texture:', error);
+                //console.error('Error loading texture:', error);
             }
         );
-    }, []); */
+    }, []);
 
-    return <canvas ref={canvasRef} style={{ background: '#000' }} />;
+    return (
+        <div className='three'>
+            <canvas ref={canvasRef} style={{ background: '#000' }} />
+        </div>
+    );
 }
